b3e46a4d1bf57e5027acf29379e14a5503b5589c
ERWANCEN <erwance@hotmail.fr>
2024-10-23 00:45:36 +0200

    commentaires code

--- suite.php
+++ suite.php
@@ -1,12 +1,22 @@
 <?php
 
-$i = 0;
-while ($i < 100) {
-    if ($i < 10) {
+echo "<h1>Boucle while</h1>";
+$i = 0; // attribution valeur 0 à la variable i
+while ($i < 100) { // boucle qui se répète tant que $i est inférieur à 100
+    if ($i < 10) { // condition "if" qui dit que si la valeur de i est inférieur à 10 alors on rajoute un 0 devant chaque chiffre
         echo "0". $i . ", ";
-        $i++;
-    } else {
+        $i++; // incrementation de 1 à la variable i à chaque répétition de la boucle
+    } else { // si valeur de i supérieure ou égale à 10 alors j'affiche juste le chiffre
         echo $i . ", ";
-        $i++;
+        $i++; // incrementation de 1 à la variable i à chaque répétition de la boucle
+    }
+}
+
+echo "<h1>Boucle for</h1>";
+for ($j = 0; $j < 100; $j++) { // attribution de 0 à la variable j; répétition boucle tant que j est inférieur à 100; incrémentation de 1 à chaque répétition de la boucle
+    if ($j < 10) { // condition qui dit que si valeur de j inférieure à 10 alors j'ajoute un 0 devant le chiffre
+        echo "0". $j . ", ";
+    } else { // si valeur de j supérieure ou égale à 10 alors afficher le chiffre seul
+        echo $j . ", ";
     }
 }
